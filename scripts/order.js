a0393a2f27595375e9aab4f6ce9d8f3432ddf58c
KhangNguyen <147159390+khangjhin4444@users.noreply.github.com>
2024-10-21 21:13:46 +0700

    Update order.js

--- scripts/order.js
+++ scripts/order.js
@@ -4,6 +4,17 @@ import { loadProductsFetch } from "../data/products.js";
 async function loadPage() {
   await loadProductsFetch();
   renderOrders();
+  document.querySelector('.search-button').addEventListener('click',() => {
+    const text = document.querySelector('.search-bar').value;
+    window.location.href = `../amazon.html?search=${text}`;
+  });
+  
+  document.querySelector('.search-bar').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const text = document.querySelector('.search-bar').value;
+      window.location.href = `../amazon.html?search=${text}`;
+    }
+  });
 }
 
 loadPage();
