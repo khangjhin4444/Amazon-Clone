ca0219bbdd23196e962ddb2cb36229cda77672fc
KhangNguyen <147159390+khangjhin4444@users.noreply.github.com>
2024-10-21 21:14:20 +0700

    Update tracking.js

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -12,6 +12,17 @@ console.log(url.searchParams.get('productId'));
 async function loadPage() {
   await loadProductsFetch();
   renderTracking();
+  document.querySelector('.search-button').addEventListener('click',() => {
+    const text = document.querySelector('.search-bar').value;
+    window.location.href = `../amazon.html?search=${text}`;
+  });
+  
+  document.querySelector('.search-bar').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const text = document.querySelector('.search-bar').value;
+      window.location.href = `../amazon.html?search=${text}`;
+    }
+  });
 }
  
 loadPage();
