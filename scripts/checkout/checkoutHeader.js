59ca48c0bcee533c8eb4fe30835e19a3b8bf2c48
KhangNguyen <147159390+khangjhin4444@users.noreply.github.com>
2024-10-21 20:43:44 +0700

    Update checkoutHeader.js

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -3,7 +3,7 @@ import {cart} from '../../data/cart-class.js';
 export function renderCheckoutHeader() {
   let html = `
     Checkout (<a class="return-to-home-link js-return-to-home-link"
-            href="amazon.html"></a>)
+            href="index.html"></a>)
   `;
   document.querySelector('.checkout-header-middle-section').innerHTML = html;
 }
